81560083a74e328121fc2aeda14b9b0812bc07e1
Peter Nalyvayko <brat000012001@gmail.com>
2025-04-03 21:49:45 -0400

    Create Asteroid_Intercept_AE508_Spring2025.m

---
+++ Asteroid_Intercept_AE508_Spring2025.m
@@ -0,0 +1,15 @@
+%
+% AE508 Optimal Space Trajectories, Spring 2025
+% Course Project
+% 
+% Team: petern4@illinois.edu, pc46@illinois.edu, davisr2@illinois.edu
+%
+
+% 
+function Xdot = eom(t,X)
+    % Xdot = [rdot; vdot; mdot];
+end
+
+function err = costFunction(t,X,T,c)
+    % err = [];
+end
