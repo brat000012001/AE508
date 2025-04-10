9ebb245181b2cc91b81ab586515cc245e0fc10e6
Peter <petern4@illinois.edu>
2025-04-10 15:58:46 +0000

    experimenting with the thrust magnitude (the last value 0.2345 from hw2/q2)

--- Asteroid_Intercept_AE508_Spring2025.m
+++ Asteroid_Intercept_AE508_Spring2025.m
@@ -4,26 +4,79 @@
 % 
 % Team: petern4@illinois.edu, pc46@illinois.edu, davisr2@illinois.edu
 %
+% troubleshooting:
+% X(340169,4:6) NaN
 
-clear;
+clear; close all; clc;
 format longg;
 
 % Calculate the state and costate differential equations
-function Xdot = eom(t,X)
+function Xdot = eom(t,X,T,c,rho,mu)
 
     r = X(1:3);
     v = X(4:6);
     m = X(7);
-    %lam = X()
+    lam_r = X(8:10);
+    lam_v = X(11:13);
+    lam_m = X(14);
 
+    % S = c/m*norm(lam_v) + lam_m - 1; % switch function
+    % delta = 0.5*(1 + tanh(S/rho));
+
+    uhat = -lam_v/norm(lam_v);
+    
     rdot = v;
+    vdot = -mu/norm(r)^3*r + T/m*uhat;
+    mdot = -T/c;
 
-    % Xdot = [rdot; vdot; mdot];
+    x = r(1);
+    y = r(2);
+    z = r(3);
+
+    scalar = -mu/((x^2+y^2+z^2)^(5/2));
+    lam_r_dot = [
+        scalar*lam_v'*[2*x^2-y^2-z^2;      3*x*y;            3*x*z];
+        scalar*lam_v'*[3*x*y; -x^2 + 2*y^2 - z^2;            3*y*z];
+        scalar*lam_v'*[3*x*z;              3*y*z;2*z^2 - x^2 - y^2]
+        ];
+
+    lam_v_dot = -lam_r;
+
+    lam_m_dot = -T/m^2*(lam_v'*uhat);
+
+    Xdot = [rdot; vdot; mdot; lam_r_dot; lam_v_dot; lam_m_dot];
 end
 
-function err = costFunction(t,X,T,c)
-    % err = [];
+function err = minFuel(lam0guess,t0,tf,x0,xf,T,c,rho,opts_ode,m0,mu)
+    [t, X] = ode45(@eom, [t0 tf], [x0; m0; lam0guess(1:7)],opts_ode,T,c,rho,mu);
+    % TODO: this needs work (the dynamic equations are not set up to do
+    % minimum fuel)
+     % err = [[X(end,1:3)';X(end,4:6)] - [xf(1:3);X(end,4:6)]; lam0guess(7)];
 end
+
+function err = minT(lam0guess,t0,x0,xf,T,c,rho,opts_ode,m0,mu)
+    tf = lam0guess(8);
+    [t, X] = ode45(@eom, [t0 tf], [x0; m0; lam0guess(1:7)],opts_ode,T,c,rho,mu);
+    H = hamiltonian(t,X,T,c,rho,mu);
+    err = [X(end,1:7)' - [xf(1:3);X(end,4:7)']; H(end) + 1];
+end
+
+function Hamiltonian = hamiltonian(t,X,T,c,rho,mu)
+    Hamiltonian = zeros(length(t),1);
+    for idx = 1:length(t)
+        r = X(idx,1:3)';
+        v = X(idx,4:6)';
+        m = X(idx,7);
+        rmag = norm(r);
+        lam_r = X(idx,8:10);
+        lam_v = X(idx,11:13);
+        lam_m = X(idx, 14);
+        uhat = -lam_v/norm(lam_v);
+        Hamiltonian(idx) = lam_r*v - lam_v*r*mu/rmag^3 + lam_v*uhat'*T/m - lam_m*T/c; 
+    end
+end
+
+mu_e = 398600.44; % the Earth's gravitational parameter 
 
 % 11 Apr 2029 00:26:00.000 UTCG is approximately the date/time when asteroid Apophis 
 % enters the Earth's SOI.The SOI was calculated using the Hill Radius (~ 234 Earth radii)
@@ -40,11 +93,158 @@ m0 = 500; % kg, the initial wet mass
 % Final condition
 % Asteroid Apophis position in Inertial reference frame centered at the
 % Earth center (ECI)
-% tf = 2462237.520833333; % A.D. 2029-Apr-11 00:30:00.0000 as Julian Date
-% TODO: guess the time of flight that makes sense given the 
-% satellite we want to hit the ateroid with is on a GEO orbit
-tf = ??? % time of flight, in seconds
 rf = [-1.096812308683544e+06, -9.292488001004120e+05, -4.114552797159857e+05]'; % the final position
 % Velocity is free at final time
 % vf = [4.206778300537007E+00, 3.801730069720992E+00, 1.639412680210433E+00]'; % the final velocity
 % mass is free at final time
+
+% tf = 2462237.520833333; % A.D. 2029-Apr-11 00:30:00.0000 as Julian Date
+theta = acos((r0/norm(r0))'*(rf/norm(rf)));
+c = sqrt(r0'*r0 + rf'*rf - 2*norm(r0)*norm(rf)*cos(theta));
+s = (norm(r0) + norm(rf) + c)/2;
+% Minimum time of flight
+t_p = sqrt(2)/(3*sqrt(mu_e))*(sqrt(s^3) - sign(sin(theta))*sqrt((s - c)^3));
+
+% The time of flight that makes sense given the 
+% satellite we want to hit the ateroid with is on a GEO orbit
+tf = t_p; % time of flight, in seconds
+
+opts_ode = odeset('RelTol',1e-13,'AbsTol',1e-15); % ode
+options = optimoptions('fsolve','Display','iter','MaxFunEvals',1e3,...
+    'MaxIter',1e3,'TolFun',1e-12,'TolX',1e-14,...
+    'UseParallel',false);
+
+T = 0.235/1000; % kN, Thrust magnitude
+Isp = 4190; % seconds, specific impulse
+g0 = 9.8; % m/s^2 (Earth's surface gravity)
+c = Isp*g0/1000; % km/s Exhaust velocity
+
+x0 = [r0;v0];
+xf = [rf;0;0;0];
+
+rho = 1.0;
+lam_guess = [0.01;0.01;0.01;0.01;0.01;0.01;0.01;tf];
+[p0,~] = fsolve(@minT,lam_guess,options,t0,x0,xf,T,c,rho,opts_ode,m0,mu_e);
+[t, X] = ode45(@eom, [t0 p0(8)], [x0; m0; p0(1:7)],opts_ode,T,c,rho,mu_e);
+H = hamiltonian(t,X,T,c,rho,mu_e);
+plot(t,H);
+plot_trajectory(t,X,r0,rf);
+plot_states(t,X);
+plot_costates(t,X);
+
+function plot_trajectory(t, X, r0, rf)
+    figure;
+    % grid on; hold on;
+    plot3(X(:,1),X(:,2),X(:,3));
+    %scatter(0,0,0,'*r'); % The origin of the central body (the Earth)
+    %scatter(r0(1),r0(2),r0(3),'*b');
+    %plot3(rf(1),rf(2),rf(3),'*g');
+    % hold off;
+end
+
+function plot_costates(t, X)
+    figure;
+    grid on; hold on;
+
+    title('Costate \lambda_r');
+    subplot(3,1,1);
+    plot(t, X(:,8),'LineWidth',1.5);
+    xlabel('t (sec)');
+    ylabel('\lambda_r_x');
+
+    subplot(3,1,2);
+    plot(t, X(:,9),'LineWidth',1.5);
+    xlabel('t (sec)');
+    ylabel('\lambda_r_y');
+
+    subplot(3,1,3);
+    plot(t, X(:,10),'LineWidth',1.5);
+    xlabel('t (sec)');
+    ylabel('\lambda_r_z');
+    hold off;
+    
+    % lambda_v
+    figure;
+    grid on; hold on;
+    title ('Costate \lambda_v');
+    subplot(3,1,1);
+    plot(t, X(:,11),'LineWidth',1.5);
+    xlabel('t (sec)');
+    ylabel('\lambda_v_x');
+
+    subplot(3,1,2);
+    plot(t, X(:,12),'LineWidth',1.5);
+    xlabel('t (sec)');
+    ylabel('\lambda_v_y');
+
+    subplot(3,1,3);
+    plot(t, X(:,13),'LineWidth',1.5);
+    xlabel('t (sec)');
+    ylabel('\lambda_v_z');
+    hold off;
+
+    % Costate lambda_m
+    figure;
+    grid on; hold on;
+    title('\lambda_m');
+    plot(t, X(:,14),'LineWidth',1.5);
+    xlabel('t (sec)');
+    ylabel('\lambda_m');
+    hold off;
+end
+
+function plot_states(t,X)
+    figure;
+    grid on; hold on;
+
+    title("Position")
+    subplot(3,1,1);
+    plot(t, X(:,1),'LineWidth',1.5);
+    xlabel('t (sec)');
+    ylabel('x (km)');
+
+    subplot(3,1,2);
+    plot(t, X(:,2),'LineWidth',1.5);
+    xlabel('t (sec)');
+    ylabel('y (km)');
+    
+    subplot(3,1,3);
+    plot(t, X(:,3),'LineWidth',1.5);
+    xlabel('t (sec)');
+    ylabel('z (km)');
+
+    hold off;
+
+    % Velocity
+    figure;
+    grid on; hold on;
+    
+    title("Velocity")
+    subplot(3,1,1);
+    plot(t, X(:,4),'LineWidth',1.5);
+    xlabel('t (sec)');
+    ylabel('v_x (km/s)');
+
+    subplot(3,1,2);
+    plot(t, X(:,5),'LineWidth',1.5);
+    xlabel('t (sec)');
+    ylabel('v_y (km/s)');
+    
+    subplot(3,1,3);
+    plot(t, X(:,6),'LineWidth',1.5);
+    xlabel('t (sec)');
+    ylabel('v_z (km/s)');
+    
+    hold off;
+
+    % Mass 
+    figure;
+    grid on; hold on;
+
+    title("Mass")
+    plot(t, X(:,7),'LineWidth',1.5);
+    xlabel('t (sec)');
+    ylabel('m (kg)');
+    
+    hold off;
+end
